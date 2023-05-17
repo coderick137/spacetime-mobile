20a5a5e122a89d6a384e4551d215ce4e3eb49ebd
coderick137 <ricardoanj.14@gmail.com>
2023-05-17 18:30:04 -0300

    setup inicial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <Text>Carregando...</Text>
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className='bg-gray-900 flex-1 items-center justify-center'>
+      <Text className='text-gray-50 text-3xl font-bold'>Lucas Moura</Text>
+      <StatusBar style="light" translucent />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
